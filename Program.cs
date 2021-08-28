2d4a6ff310d06cb4d3ab825d363955c78948deed
João Paulo <joaopdejesus79@gmail.com>
2021-08-27 21:26:59 -0300

    Programa Finalizado!

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AsciiArt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine(" ");
+            Console.WriteLine(" ||  ||");
+            Console.WriteLine(" \\\\()//");
+            Console.WriteLine("//(__)\\\\");
+            Console.WriteLine("||    ||");
+            Console.WriteLine(" ");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
